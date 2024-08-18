9facf043e7a25af8c5c89822e7b002a18b7a49b3
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-08-18 09:51:37 +0300

    authroute

---
+++ routes/authRoute.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const authController= require('../controllers/authController')
+
+
+router.post('/signup', authController.signup_post)
+router.post('/login', authController.login_post)
+module.exports = router
