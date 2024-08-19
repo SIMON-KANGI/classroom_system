6736f674e41d19f0bca82d954aef86904294efeb
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-08-19 23:04:34 +0300

    server

--- server.js
+++ server.js
@@ -2,13 +2,18 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookie=require('cookie-parser')
 const app = express();
-const cors= require('cors')
 require('dotenv').config();
-app.use(cors())
+const cors= require('cors')
+
+const corsOptions = {
+  origin: 'http://localhost:5173', 
+  credentials: true, 
+};
+app.use(cors(corsOptions))
 app.use(express.json());
 app.use(cookie());
 
-const dbURI='mongodb+srv://7889kangi:7889kangi@cluster0.od2vjow.mongodb.net/'
+const dbURI='mongodb+srv://simonmwangikangi:7889Kangi@cluster0.czigckw.mongodb.net/'
 mongoose.connect(dbURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -16,18 +21,22 @@ mongoose.connect(dbURI, {
 }).then((res) => console.log('successfully connected to db'))
 .catch((err) => {
   console.error('Error connecting to MongoDB:', err.message);
-  process.exit(1); // Exit the process with an error code
+  process.exit(1); 
 });
 
 const userRoute=require('./routes/userRoute')
 const authRoute=require('./routes/authRoute')
+const classRoute= require('./routes/classRoute')
+const timetableRoute=require('./routes/timetableRoute')
 
 app.use('/api', userRoute);
 app.use('/api', authRoute)
+app.use('/api', classRoute);
+app.use('/api', timetableRoute)
 
 
+const PORT = process.env.PORT || 3000;
 
-
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('Server is running on port 3000');
 });
