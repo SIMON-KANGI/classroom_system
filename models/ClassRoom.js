0fcbfa0f40a6314a5bddfab1c844d26e516b1a25
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-08-19 23:01:26 +0300

    classroom model

--- models/ClassRoom.js
+++ models/ClassRoom.js
@@ -1,37 +1,27 @@
-const mongoose= require('mongoose');
-const Schema=  mongoose.Schema
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const ClassRoomSchema= new Schema({
-    name:{
-        type:String,
-        required:true
+const ClassRoomSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  schedule: [{
+    day: {
+      type: String,
+      required: true,
     },
-    students: [{
-        type: Schema.Types.ObjectId,
-        ref: 'User',  // References to the Student model
-    }],
-    teacher:{
-        type: Schema.Types.ObjectId,
-        ref: 'User',  // References to the Teacher model
+    startTime: {
+      type: String,
+      required: true,
     },
-    schedule: [{
-        day: {
-          type: String,
-          required: true,
-        },
-        subject: {
-          type: String,
-          required: true,
-        },
-        startTime: {
-          type: String,
-          required: true,
-        },
-        endTime: {
-          type: String,
-          required: true,
-        }
-      }]
-})
+    endTime: {
+      type: String,
+      required: true,
+    },
+   
+  }],
+  
+});
 
-module.exports= mongoose.model('ClassRoom', ClassRoomSchema)
+module.exports = mongoose.model('ClassRoom', ClassRoomSchema);
