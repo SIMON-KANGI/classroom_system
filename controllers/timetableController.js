818814cc7c8bb2776ab04dad0feb5c6a4bcc963c
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-08-19 23:02:01 +0300

    timetablecontroller

--- controllers/timetableController.js
+++ controllers/timetableController.js
@@ -1,38 +1,63 @@
-const Timetable= require('../models/Timetable')
-const Classroom= require('../models/Classroom')
-exports.getAllTimetables=async(req,res)=>{
-    try{
-        const timetables= await Timetable.find()
-        res.json(timetables)
-    }catch(err){
-        res.status(500).json({message:err.message})
+const Timetable = require('../models/Timetable');
+const Classroom = require('../models/ClassRoom');
+
+// Get all timetables
+exports.getAllTimetables = async (req, res) => {
+    try {
+        const timetables = await Timetable.find().populate('classroom'); // Populating referenced documents
+        res.json(timetables);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
     }
-}
+};
 
-exports.createTimeTable= async(req,res)=>{
-    const timetable= new Timetable({classroomName, schedule})
-
-    try{
+// Create a new timetable
+exports.createTimeTable = async (req, res) => {
+    const { classroomName, schedule, teacher } = req.body;
+    try {
+        // Find the classroom by name
         const classroom = await Classroom.findOne({ name: classroomName });
-        if(!classroom) return "Create a classroom first"
-        const newTimetable= new Timetable({
-            classroom:classroom._id,
-            schedule
-        })
-        newTimetable.save()
-        res.status(201).json(newTimetable)
-    }catch(err){
-        res.status(400).json({message:err.message})
-    }
-}
-exports.getTimeTableById= async(req,res)=>{
-    try{
-        const timetable= await Timetable.findById(req.params.id)
-        if(!timetable){
-            return res.status(404).json({message:'Cannot find timetable'})
+        if (!classroom) {
+            return res.status(400).json({ message: 'Classroom not found. Create a classroom first.' });
         }
-        res.json(timetable)
-    }catch(err){
-        res.status(500).json({message:err.message})
+
+        // Create a new timetable
+        const newTimetable = new Timetable({
+            classroom: classroom._id,
+            schedule,
+            teacher,
+        });
+
+        //save
+        await newTimetable.save();
+        res.status(201).json(newTimetable);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
     }
-}
+};
+
+// Get a timetable by ID
+exports.getTimeTableById = async (req, res) => {
+    try {
+        const timetable = await Timetable.findById(req.params.id).populate('classroom teacher');
+        if (!timetable) {
+            return res.status(404).json({ message: 'Cannot find timetable' });
+        }
+        res.json(timetable);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+// Delete a timetable by ID
+exports.deleteTimeTable = async (req, res) => {
+    try {
+        const timetable = await Timetable.findByIdAndDelete(req.params.id);
+        if (!timetable) {
+            return res.status(404).json({ message: 'Cannot find timetable' });
+        }
+        res.json({ message: 'Timetable deleted' });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
